40e262499c327e0fd5d80912f0c10d1d641cdf91
KarinShoji <36564217+sika0115@users.noreply.github.com>
2021-12-14 18:24:59 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import "./styles.css";
+//then:取得後に実行する関数書く、catch:エラー時に実行する関数を書く
+export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>user(id:1)</button>
+    </div>
+  );
+}
